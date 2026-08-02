102b9350afdd51336417d9ecc5128494d25a4ee1
Giftzwerg02 <chessplayer1@gmx.at>
2026-08-02 21:07:58 +0200

    Increment mouse-wheel instead of overriding to reflect

--- examples/SDL3-simple-demo/main.c
+++ examples/SDL3-simple-demo/main.c
@@ -161,7 +161,8 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
             Clay_SetLayoutDimensions((Clay_Dimensions) { (float) event->window.data1, (float) event->window.data2 });
             break;
         case SDL_EVENT_MOUSE_WHEEL:
-            state->mouseWheel = (Clay_Vector2) { event->wheel.x, event->wheel.y };
+            state->mouseWheel.x = event->wheel.x;
+            state->mouseWheel.y = event->wheel.y;
             break;
         default:
             break;
